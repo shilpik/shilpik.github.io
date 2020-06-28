9dd0106f0a9d40b1c507a57b05934dd09c0e5c56
shilpik <shilpi.lal.kumar@gmail.com>
2020-06-28 14:58:32 -0700

    syncing

---
+++ index/logbook.js
@@ -0,0 +1,31 @@
+function myFunctionamy() {
+  var x =
+  document.getElementById("panelamy")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctionalicia() {
+  var x =
+  document.getElementById("panelalicia")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunctionkytana() {
+  var x =
+  document.getElementById("panelkytana")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
