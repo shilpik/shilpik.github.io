1f9a5105e4b8721ea3a18c980e1fb4d2a1cbde61
shilpik <shilpi.lal.kumar@gmail.com>
2020-06-29 19:04:24 -0700

    testing meta + artist page

--- index/logbook.js
+++ index/logbook.js
@@ -18,10 +18,40 @@ function myFunctionalicia() {
     x.style.display = "block";
   }
 }
+function myFunctionartalicia() {
+  var x =
+  document.getElementById("panelartalicia")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
 function myFunctionkytana() {
   var x =
   document.getElementById("panelkytana")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctionartkytana() {
+  var x =
+  document.getElementById("panelartkytana")
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunctionartamy() {
+  var x =
+  document.getElementById("panelartamy")
 
   if (x.style.display === "block") {
     x.style.display = "none";
